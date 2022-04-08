440655c60a1d8e989354a9ac2a1c8c9e94824fda
EsQLap <61599652+EsQLap@users.noreply.github.com>
2022-04-08 12:49:25 +0400

    Исправлено наследование BoxDto

--- example/src/main/java/ru/virgil/example/box/BoxDto.java
+++ example/src/main/java/ru/virgil/example/box/BoxDto.java
@@ -2,11 +2,11 @@ package ru.virgil.example.box;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import ru.virgil.example.system.IdentifiedEntity;
+import ru.virgil.example.system.IdentifiedDto;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
-public class BoxDto extends IdentifiedEntity {
+public class BoxDto extends IdentifiedDto {
 
     private BoxType type;
     private String description;
