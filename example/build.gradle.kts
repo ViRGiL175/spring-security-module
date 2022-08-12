6852e7c345f1097300cf66a71d8df32bdb4e9d74
ViRGiL175 <virgil7g@gmail.com>
2022-08-12 10:24:34 +0400

    Update Gradle and DI library

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     id("org.springframework.boot") version "2.6.10"
-    id("io.spring.dependency-management") version "1.0.12.RELEASE"
+    id("io.spring.dependency-management") version "1.0.13.RELEASE"
     id("java")
 }
 
