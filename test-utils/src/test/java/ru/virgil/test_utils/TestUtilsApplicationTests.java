baf352bd4dbb54a4c90c36d374e538f43122e9b9
ViRGiL175 <virgil7g@gmail.com>
2023-01-29 02:42:12 +0400

    Fix context testing

--- test-utils/src/test/java/ru/virgil/test_utils/TestUtilsApplicationTests.java
+++ test-utils/src/test/java/ru/virgil/test_utils/TestUtilsApplicationTests.java
@@ -1,8 +1,10 @@
 package ru.virgil.test_utils;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@AutoConfigureMockMvc
 @SpringBootTest
 class TestUtilsApplicationTests {
 
